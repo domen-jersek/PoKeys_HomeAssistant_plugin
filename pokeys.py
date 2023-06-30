3558e167b917cc966f193ad3f58dea0c1587a75f
domen jersek <domenjersek@gmail.com>
2023-06-30 09:18:59 +0200

    serial number parameter

--- pokeys.py
+++ pokeys.py
@@ -1,27 +1,30 @@
 import asyncio
 import logging
-from homeassistant.const import (CONF_NAME, CONF_HOST)
+from homeassistant.const import (CONF_NAME) #, CONF_SERIAL
 from .pokeys_interface import pokeys_interface
 
 pk = pokeys_interface()
 LOGGER = logging.getLogger(__name__)
+CONF_SERIAL = "serial"
 
 class pokeys_instance:
-    def __init__(self, host: str):
-        self._host = host
+    def __init__(self, serial: str):
+        self._serial = serial
+        host = pk.device_discovery(serial)
         self._device = pk.connect(host)
         self._is_on = None
         self._connected = None
 
     async def _send(self):
-        if not pk.connect(_host):
+        host = pk.device_discovery(_serial)
+        if not pk.connect(host):
             LOGGER.error("Not available")
         else:
             LOGGER.error("Device name " + pk.get_name())
             LOGGER.error(pk.inputs)
 
-    def host(self):
-        return self._host
+    def serial(self):
+        return self._serial
     
     def is_on(self):
         return self._is_on
